cb3d613ab039f8fd3c6807e1b687b9e49610725a
Philipp Schmitt <philipp@schmitt.co>
2026-08-15 10:43:41 +0200

    style: apply ci formatter patch

--- app/src/main/kotlin/dev/pschmitt/syncwich/ui/home/HomeScreen.kt
+++ app/src/main/kotlin/dev/pschmitt/syncwich/ui/home/HomeScreen.kt
@@ -18,8 +18,8 @@ import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.History
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.NewReleases
 import androidx.compose.material.icons.filled.Restaurant
 import androidx.compose.material.icons.filled.RestaurantMenu
